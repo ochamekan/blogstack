5da488be44c303a58964cee0dcf2e7eb3e8efb66
ochamekan <yanikidev@gmail.com>
2026-04-03 21:59:54 +0200

    minor

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
 
+from src.deps import SessionDep
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/login")
-async def login():
+async def login(session: SessionDep):
     pass
 
 
 @router.post("/signup")
-async def signup():
+async def signup(session: SessionDep):
     pass
